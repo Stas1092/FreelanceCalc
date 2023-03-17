ff1c830d4650731696107cd45dddfb052457b609
Stas1092 <ss9262694394@gmail.com>
2023-03-17 14:36:45 +0300

    Code formatting

--- src/main/java/ru/netology/services/Main.java
+++ src/main/java/ru/netology/services/Main.java
@@ -2,8 +2,8 @@ package ru.netology.services;
 
 public class Main {
     public static void main(String[] args) {
-    FreelanceCalc service = new FreelanceCalc();
-    int freeMontes = service.calc(100_000, 60_000, 150_000);
-    System.out.println(freeMontes);
+        FreelanceCalc service = new FreelanceCalc();
+        int freeMontes = service.calc(100_000, 60_000, 150_000);
+        System.out.println(freeMontes);
     }
 }
